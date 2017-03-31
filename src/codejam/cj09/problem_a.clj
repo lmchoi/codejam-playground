32f0e316542d45720cbb814915858bc0f3189fb4
Mandy Choi <hello@mandychoi.com.au>
2017-03-31 01:33:41 +0100

    2009 QR problem A - deleted redundant code

--- src/codejam/cj09/problem_a.clj
+++ src/codejam/cj09/problem_a.clj
@@ -13,15 +13,13 @@
   (map token->possible-letters (re-seq #"\(.*?\)|[a-z]" pattern)))
 
 (defn- match? [word-seq tokens]
-  (let [r (mapv (fn [w t]
-                  (contains? t w)) word-seq tokens)]
-    (loop [ts tokens
-           ws word-seq]
-      (if ts
-        (if (contains? (first ts) (first ws))
-          (recur (next ts) (next ws))
-          false)
-        true))))
+  (loop [ts tokens
+         ws word-seq]
+    (if ts
+      (if (contains? (first ts) (first ws))
+        (recur (next ts) (next ws))
+        false)
+      true)))
 
 (defn num-of-words-that-match
   [words pattern]
@@ -33,11 +31,6 @@
             0 words)))
 
 (defn problem-a []
-; The alien language is very unique in that every word consists of exactly L lowercase letters. Also, there are exactly D words in this language.
-; some of the words may be misinterpreted
-; determine the number of possible interpretations for a given pattern.
-;A pattern consists of exactly L tokens
-
   (let [[l d n] (u/read-values)]
     (let [words (sort (u/read-lines d))]
       (doseq [i (range 1 (inc n))]
